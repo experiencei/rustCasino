48d807febc3d7f73f8fef61eca657830aa3f42f2
experiencei <ayelojahighbee01@gmail.com>
2022-03-01 21:36:44 +0100

    working with loops and mut

--- match.rs
+++ match.rs
@@ -13,10 +13,26 @@ fn main () {
 
 fn main () {
     let some_int = 1;
+
     match some_int {
         1 => println!("its 1"),
         2 => println!("its 2"),
         3 => println!("its 3"),
         _ => println!("its something else")
     }
+}
+
+// Decision making with loops
+
+fn main () {
+    let mut i =3;
+
+    loop {
+        println!("{:?}" , i);
+        i = i - 1;
+        if i == 0 { 
+            break;
+        }
+    }
+  println!("Done!")
 }
