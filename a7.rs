ee2fe3082a51a73df8ccedb7a9b2fd5451831841
experiencei <ayelojahighbee01@gmail.com>
2022-03-03 09:29:32 +0100

    working with enum keywords

---
+++ a7.rs
@@ -0,0 +1,20 @@
+// Topic: Working with an enum
+
+enum Color {
+    Red, 
+    Green,
+    Yellow,
+    Black,
+    Blue,
+}
+
+fn main() {
+    let color = Color::Red;
+    match color {
+        Color::Red => println!("it's red"),
+        Color::Green = println!("it's green"),
+        Color::Yellow = println!("it's yellow"),
+        Color::Black => println!("it's black"),
+        Color::Blue = println!("it's blue"),
+    }
+}
