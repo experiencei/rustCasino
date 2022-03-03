dca3bf7bad07186e6c98e7032a30370d5bb60e40
experiencei <ayelojahighbee01@gmail.com>
2022-03-03 08:53:49 +0100

    working on repitition using logic

---
+++ a5.rs
@@ -0,0 +1,19 @@
+fn main() {
+// * Use a mutable integer variable
+
+let mut v = 1;
+
+// * Use a loop statement
+loop {
+ // * Print the variable within the loop statement
+  println!("{;?}", v);
+
+  if v == 4 { 
+      println("{;?}", v);
+      break
+    }
+// * Use break to exit the loop
+v += 1
+}
+}
+
