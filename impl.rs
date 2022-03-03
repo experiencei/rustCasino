2d6dd21a97ed317c7be736a211fe5506b0fa641c
experiencei <ayelojahighbee01@gmail.com>
2022-03-03 23:37:27 +0100

    working with ownerships

---
+++ impl.rs
@@ -0,0 +1,27 @@
+struct Temperature {
+    degree_f : f64,
+}
+
+impl Temperature {
+    fn freezing() -> Self {
+        Self {
+            degree_f : 32
+        }
+    }
+    fn show_temp(&self) {
+        println!("{:?} degree F" , self.degree_f);
+    }
+}
+
+
+fn main() {
+    let hot = Temperature {
+        degree_f : 99.9
+    };
+
+
+    hot.show_temp();
+    // Temperature::show_temp(hot);
+    let cold = Temperature::freezing();
+     cold.show_temp();
+}
