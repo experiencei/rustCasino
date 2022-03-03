e6e2c520926995eaaaa6b03a5aa4fac458fe4291
experiencei <ayelojahighbee01@gmail.com>
2022-03-03 10:32:49 +0100

    working with struct and enum

--- a8.rs
+++ a8.rs
@@ -1,35 +1,60 @@
-enum Flavors {
-   Banana,
-   Vanilla,
-}
+// enum Flavors {
+//    Banana,
+//    Vanilla,
+// }
 
-struct DrinkFlavor {
-   flavors : Flavors,
-   fluid_oz : f64,
-}
+// struct DrinkFlavor {
+//    flavors : Flavors,
+//    fluid_oz : f64,
+// }
 
-fn print_drink( juicy : DrinkFlavor) {
+// fn print_drink( juicy : DrinkFlavor) {
    
-    match juicy.flavors {
-       Flavor::Banana => println!("flavour : banana"),
-       Flavor::Vanilla => println!("flavour : vanilla"),
+//     match juicy.flavors {
+//        Flavor::Banana => println!("flavour : banana"),
+//        Flavor::Vanilla => println!("flavour : vanilla"),
+//     }
+
+//     println!("fluid_oz" , juicy.fluid_oz);
+// }
+
+// fn main() {
+//     let first_drink = DrinkFlavor {
+//         flavors: Flavor::Banana,
+//         fluid_oz: 0.9
+//     }
+
+//     print_drink(first_drink);
+
+//     let second_drink = DrinkFlavor {
+//         flavors: Flavor::Vanilla,
+//         fluid_oz: 8.10
+//     }
+
+//     print_drink(second_drink);
+// }
+
+enum Flavor {
+    Sparklings,
+    Fruity,
+}
+
+struct Drink {
+    flavor: Flavor,
+    fluid_oz: f64,
+
+}
+
+fn print_drink(drink: Drink) {
+   
+    match drink.flavor {
+         Flavor::Sparklings => println!("flavor : sparklings")
+         Flavor::Fruity => println!("flavor : fruity")
     }
 
-    println!("fluid_oz" , juicy.fluid_oz);
+    println!("fluid_oz" , drink.fluid_oz);
 }
 
 fn main() {
-    let first_drink = DrinkFlavor {
-        flavors: Flavor::Banana,
-        fluid_oz: 0.9
-    }
-
-    print_drink(first_drink);
-
-    let second_drink = DrinkFlavor {
-        flavors: Flavor::Vanilla,
-        fluid_oz: 8.10
-    }
-
-    print_drink(second_drink);
+    let pass = Drink
 }
