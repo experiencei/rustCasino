7e041aab0c38488a2f9f8d57416d4982990a3bf8
experiencei <ayelojahighbee01@gmail.com>
2022-03-03 08:58:03 +0100

    working on repitition using logic

--- a5.rs
+++ a5.rs
@@ -6,10 +6,10 @@ let mut v = 1;
 // * Use a loop statement
 loop {
  // * Print the variable within the loop statement
-  println!("{;?}", v);
+  println!("{:?}", v);
 
   if v == 4 { 
-      println("{;?}", v);
+      println("{:?}", v);
       break
     }
 // * Use break to exit the loop
@@ -17,3 +17,13 @@ v += 1
 }
 }
 
+// repitition using while loop
+
+fn main() {
+    let mut x = 1;
+
+    while x <= 3 {
+        println!("{:?}", x);
+        x += 1;
+    }
+}
