28afc27cb7c6a1f89ae7e690d6e865490940eda1
experiencei <ayelojahighbee01@gmail.com>
2022-03-04 16:08:49 +0100

    activity with impl keywords

--- impl.rs
+++ impl.rs
@@ -1,29 +1,12 @@
 struct Temperature {
-    degree_f : f64,
+    degree_f : i32,
 }
 
 
-
-impl Temperature {
-    fn freezing() -> Self {
-        Self {
-            degree_f : 32
-        }
-    }
-    fn show_temp(&self) {
-        println!("{:?} degree F" , self.degree_f);
-    }
+fn show_Temperature(temp : Temperature)  {
+   println!(" degree in celsius is {:?}" , temp.degree_f);
 }
-
 
 fn main() {
-    let hot = Temperature {
-        degree_f : 99.9
-    };
 
-
-    hot.show_temp();
-    // Temperature::show_temp(hot);
-    let cold = Temperature::freezing();
-     cold.show_temp();
 }
