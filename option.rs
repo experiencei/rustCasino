c52452627bce7ec732a5700f74ab3bc7891e3bad
experiencei <ayelojahighbee01@gmail.com>
2022-03-04 19:59:09 +0100

    working with option for varying value

--- option.rs
+++ option.rs
@@ -19,3 +19,26 @@ fn main() {
        None => println!("age is not provided")
    }
 }
+
+//return used  below is for early return to avoid looping through the entire vector
+
+
+struct GroceryItem {
+    name: String,
+    qty : i32
+}
+
+fn find_item(name: &str) -> Option<i32> {
+    let groceries = vec![
+        GroceryItem { name :"banana".to_owned(), qty : 5},
+        GroceryItem { name :"eggs".to_owned(), qty : 25},
+        GroceryItem { name :"bread".to_owned(), qty : 2},
+    ];
+
+    for item in groceries {
+        if item.name == name {
+            return Some(item.qty);
+        }
+    }
+    None
+}
