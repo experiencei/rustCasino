358e722d460cd0384a383e85657cf6368e0cb28c
experiencei <ayelojahighbee01@gmail.com>
2022-03-04 17:00:47 +0100

    working with vectors

---
+++ vector.rs
@@ -0,0 +1,14 @@
+struct Test {
+    score : i32,
+}
+
+let my_score = vec![
+    Test { score: 66},
+    Test { score: 77},
+    Test { score: 88 },
+    Test { score: 99 },
+]
+
+for test in my_score {
+    println!(" score = {:?}", test.score)
+}
