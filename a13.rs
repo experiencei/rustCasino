ce302ffe8e024cbc3d0ad06a6dc2817c7a06bd70
experiencei <ayelojahighbee01@gmail.com>
2022-03-04 17:08:15 +0100

    working with vectors

--- a13.rs
+++ a13.rs
@@ -1,22 +1,36 @@
-struct Numbers {
-   number : 132
-}
+// struct Numbers {
+//    number : 132
+// }
 
-struct String {
-    letters : String
-}
+// struct String {
+//     letters : String
+// }
 
-let numbers = vec![
-    Numbers { number : 10},
-    Numbers { number : 20 },
-    String { letters : "thirty"},
-    Numbers { number : 40},
-];
+// let numbers = vec![
+//     Numbers { number : 10},
+//     Numbers { number : 20 },
+//     String { letters : "thirty"},
+//     Numbers { number : 40},
+// ];
 
-for num_let in numbers {
-     println!()
-}
+// for num_let in numbers {
+//      println!()
+// }
+
+// fn main() {
+
+// }
+
+
 
 fn main() {
+    let my_numbers = vec![10, 20, 30, 40];
+    for num in &my_numbers {
+        match num {
+            30 => println!("thirty"),
+            _ => println!("{:?}", num),
+        }
+    }
 
+    println!("number of elements = {:?}", my_numbers.len());
 }
