d326e22334c760e9bf4d9d374d96a4a5613574d2
experiencei <ayelojahighbee01@gmail.com>
2022-03-04 17:37:41 +0100

    working with String &str for demo activity

--- string.rs
+++ string.rs
@@ -10,3 +10,35 @@ fn main() {
     println!(&owned_string);
     println!(&another owned_string);
 }
+
+// demonstration on string slice
+
+struct Lineitem {
+    name  : String,
+    count : i36,
+}
+
+
+func main() {
+  fn print_data(data: &str) {
+      println!("{:?}", data);
+  }
+
+
+    let receipt = vec![
+        Lineitem {
+            name : "Ayeloja".to_owned(),
+            count : 1,
+        } ,
+        Lineitem {
+            name : String::from("Ibrahim"),
+            count : 3,
+        }
+    
+    ]
+    
+    for item in receipt {
+        print_data(&item.name);
+        println!("{:?}", item.count);
+    }
+}
