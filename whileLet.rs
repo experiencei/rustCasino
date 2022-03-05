8fe57b0e9bb50cf9273ba5c355196134563f1302
experiencei <ayelojahighbee01@gmail.com>
2022-03-05 17:32:37 +0100

    if let keywords for a variable instead of match

---
+++ whileLet.rs
@@ -0,0 +1,18 @@
+ fn main() {
+
+    let mut data = Some(3);
+
+    while let Some(i) = data {
+        println!("loop");
+        data = None;
+    }
+
+    let numbers = vec![1, 2, 3, 4];
+    let mut numbers_iter = numbers.iter();
+    while let Some(i) = numbers_iter.next() {
+          println("num = {:?}", i);
+    }
+    println!("Done!")
+  }
+
+  // .next() allow the looping to return an optional value as long as there are value to return
