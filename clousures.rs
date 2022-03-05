409a85da8c58f58d4938eefd50baef2a54841e88
experiencei <ayelojahighbee01@gmail.com>
2022-03-05 15:27:45 +0100

    map combinator instead of closures

--- clousures.rs
+++ clousures.rs
@@ -1,4 +1,21 @@
 fn main() {
     let add = | a , b| a + b;
+
+}
+
+fn maybe_num() -> Option<i32> {
     
+}
+
+fn maybe_word() -> Option<String> {
+
+}
+
+fn main2() {
+    let word_length = maybe_word()
+             .map(|word| word.len())
+             .map(|len| len * 2);
+
+    let plus_one = maybe_num()
+             .map(|num| num + 1);
 }
